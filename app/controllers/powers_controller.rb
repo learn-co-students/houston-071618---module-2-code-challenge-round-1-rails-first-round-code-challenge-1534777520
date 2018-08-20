f3de5d6ea737e4eead0ec40fb04db3c0956a89b7
Jessica Castaneda <pulsar.ppz70m@gmail.com>
2018-08-20 11:27:37 -0500

    Code Challenge

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -2,4 +2,18 @@ class PowersController < ApplicationController
   def index
     @powers = Power.all
   end
+
+  def show
+    @power = Power.find(params[:id])
+  end
+
+  def new
+    @power = Power.new
+  end
+
+  def create
+    #power = Power.find(params[:id])
+  end
+
+
 end
