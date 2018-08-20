bb62ebe4ae3d10e589d3f8ea4358e3bee3023dfd
github username <github email address>
2018-08-20 11:05:23 -0500

    done

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,53 @@
 class HeroinesController < ApplicationController
+  before_action :set_heroine, only: [:show, :edit, :update, :destroy]
+
   def index
-    @heroines = Heroine.all
+    # @heroines = Heroine.all
+    @heroines = Heroine.search(params[:search])
   end
+
+  def show
+    # byebug
+  end
+
+  def new
+    @heroine = Heroine.new
+  end
+
+  def edit
+  end
+
+  def create
+    @heroine = Heroine.new(heroine_params)
+
+    if @heroine.save
+      redirect_to heroine_path(@heroine)
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @heroine.update(heroine_params)
+      redirect_to @heroine
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @heroine.destroy
+
+    redirect_to heroines_path
+  end
+
+  private
+
+    def set_heroine
+      @heroine = Heroine.find(params[:id])
+    end
+
+    def heroine_params
+      params.require(:heroine).permit(:name, :super_name, :power_id, :search)
+    end
 end
--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,5 +1,52 @@
 class PowersController < ApplicationController
-  def index
-    @powers = Power.all
+    before_action :set_power, only: [:show, :edit, :update, :destroy]
+
+    def index
+      @powers = Power.all
+    end
+
+    def show
+      # byebug
+    end
+
+    def new
+      @power = Power.new
+    end
+
+    def edit
+    end
+
+    def create
+      @power = Power.new(power_params)
+
+      if @power.save
+        redirect_to power_path(@power)
+      else
+        render :new
+      end
+    end
+
+    def update
+      if @power.update(power_params)
+        redirect_to @power
+      else
+        render :edit
+      end
+    end
+
+    def destroy
+      @power.destroy
+
+      redirect_to powers_path
+    end
+
+    private
+
+      def set_power
+        @power = Power.find(params[:id])
+      end
+
+      def power_params
+        params.require(:power).permit(:name, :description)
+      end
   end
-end
