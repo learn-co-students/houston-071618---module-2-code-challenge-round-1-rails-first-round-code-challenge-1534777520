be2a3ac19ad3eaa84d7bad987929586831ed236a
Marylene Chan <marylenechan@Marylenes-MacBook-Pro-2.local>
2018-08-20 11:21:40 -0500

    Done

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,57 @@
 class HeroinesController < ApplicationController
+  before_action :select_heroine, only: [:show, :edit, :update, :destroy]
+
   def index
     @heroines = Heroine.all
   end
+
+  def show
+  end
+
+  def new
+    @heroine = Heroine.new
+  end
+
+  def create
+    @heroine = Heroine.new(params_heroine)
+
+    if @heroine.valid?
+    @heroine.save
+    redirect_to heroine_path(@heroine)
+    else
+    render :new
+    end
+
+  end
+
+  def search
+    # raise "some search magic"
+    if Heroine.find_by(:power_id == :power_id)
+    @heroine
+    # raise "true"
+    end
+
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def params_heroine
+    params.require(:heroine).permit(:name, :super_name, :power_id)
+  end
+
+  def select_heroine
+    @heroine = Heroine.find(params[:id])
+  end
+
+
+
 end
--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,5 +1,37 @@
 class PowersController < ApplicationController
+  before_action :select_power, only: [:show, :edit, :update, :destroy]
+
   def index
     @powers = Power.all
   end
+
+  def show
+  end
+
+  def new
+    @power = Power.new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def select_power
+    @power = Power.find(params[:id])
+  end
+
+  def params_power
+    params.require(:power).permit(:name, :description)
+  end
+
 end
