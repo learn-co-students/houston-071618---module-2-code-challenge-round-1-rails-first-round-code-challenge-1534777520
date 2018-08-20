24fa67d5dfedff2f34b98c44447b450d0b8f5da7
github username <github email address>
2018-08-20 10:59:08 -0500

    Passing all tests

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,57 @@
 class HeroinesController < ApplicationController
+  before_action :load_heroine
+  
   def index
     @heroines = Heroine.all
+  end
+  
+  def show
+  end
+  
+  def new
+    @power_options = Power.all.map {|power| [power.name, power.id]}
+  end
+  
+  def create
+    heroine = Heroine.new(heroine_params)
+    
+    if heroine.save
+      heroine.save
+      redirect_to heroine
+    else
+      # byebug
+      @power_options = Power.all.map {|power| [power.name, power.id]}
+      render :new 
+    end
+  end
+  
+  def edit 
+    @power_options = Power.all.map {|power| [power.name, power.id]}
+  end 
+  
+  def update
+    @heroine.update(heroine_params)
+    
+    redirect_to @heroine
+  end 
+  
+  def destroy
+    @heroine.destroy
+    
+    redirect_to heroines_path
+  end
+  
+  private
+  
+  def load_heroine
+    if params[:id] != nil
+      @heroine = Heroine.find(params[:id])
+    else
+      @heroine = Heroine.new
+    end
+  end
+  
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name, :power_id)
   end
 end
